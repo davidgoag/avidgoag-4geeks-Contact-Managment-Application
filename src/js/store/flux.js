3c38564a68509d52bb7c518aac52bb231ab75177
David Gómez <davidgoag1@gmail.com>
2021-10-20 19:06:48 +0000

    getCOntacts connected with API

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -2,10 +2,23 @@ const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
 			//Your data structures, A.K.A Entities
+			contacts: []
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+			getContacts: () => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/davidgoag")
+					.then(response => {
+						if (response.ok) {
+							return response.json();
+						} else {
+							return new Error("Error fetching the API info");
+						}
+					})
+					.then(data => setStore(data))
+					.catch(error => console.error(error));
+			}
 		}
 	};
 };
